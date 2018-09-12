e7667327a83b5dc51fe995317e15c695c8af881f
unknown <andres@digitalhouse.com>
2018-09-11 22:29:54 -0300

    Aula18

--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
@@ -1,13 +1,28 @@
 package br.com.digitalhouse.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private TextView textViewRegiaterNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textViewRegiaterNow = (TextView) findViewById(R.id.rgs_register);
+
+        textViewRegiaterNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
